21bbaabc3092003cf4dab830138e0ad4ea121d4d
Ghislain Bernard <ghislain.bernard@gmail.com>
2025-10-27 02:12:51 +0100

    Update 'portal/views.py'

--- portal/views.py
+++ portal/views.py
@@ -3,9 +3,13 @@
 import django.http
 import django.shortcuts
 
+import portal.models
+
 #----------------------------------------------------------------------------------------------------------------------#
 
 def home(request: django.http.HttpRequest):
-  return django.shortcuts.render(request, 'index.html')
+  return django.shortcuts.render(request, 'index.html', {
+    'elements': portal.models.Element.objects.all(),
+  })
 
 #--------------------------------------------- ghislain.bernard@gmail.com ---------------------------------------------#
