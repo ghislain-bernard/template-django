1477d1cbb7281481552ff06fd42082183cef5bd1
Ghislain Bernard <ghislain.bernard@gmail.com>
2025-10-27 02:12:50 +0100

    Update 'project/urls.py'

--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,17 @@
 #--------------------------------------------- ghislain.bernard@gmail.com ---------------------------------------------#
 
+import django.contrib.admin
+import django.shortcuts
 import django.urls
 
 import portal.views
 
 #----------------------------------------------------------------------------------------------------------------------#
 
-urlpatterns = [django.urls.path('', portal.views.home)]
+urlpatterns = [
+  django.urls.path('admin/', django.contrib.admin.site.urls),
+  django.urls.path('favicon.ico', lambda _ : django.shortcuts.redirect('static/favicon.ico', permanent=True)),
+  django.urls.path('', portal.views.home, name='home'),
+]
 
 #--------------------------------------------- ghislain.bernard@gmail.com ---------------------------------------------#
